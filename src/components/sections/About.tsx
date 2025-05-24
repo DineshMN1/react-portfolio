cfbfef20bb026cecce8e143d1feeee0e3b0b7009
Dinesh M <dineshmuthuswami@gmail.com>
2025-05-24 12:31:45 +0530

    Revert "update profile"
    This reverts commit fa335f6ee0f6c1d4145406003ae1e3b4269606de.

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,44 +1,19 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 export const About = () => {
-    const Fullstack = [
+    const frontendSkills = [
         "React",
         "Next.js",
         "TypeScript",
         "Tailwind CSS",
-        "Express",
+    ];
+
+    const backendSkills = [
+        "Node.js",
+        "python",
+        "Express.js",
+        "AWS",
         "MongoDB",
         "PostgreSQL",
-        "Git",
-        "Firebase",
-    ];
-
-    const Blockchain = [
-        "Solidity",
-        "Ethereum",
-        "Smart Contracts",
-        "Web3.js",
-        "Ethers.js",
-        "Hardhat",
-        "Truffle",
-        "Ganache",
-        "IPFS",
-        "Solana",
-        "Rust",
-        "Anchor",
-        
-    ];
-
-    const Devops = [
-        "Docker",
-        "Kubernetes",
-        "CI/CD",
-        "Git",
-        "GitHub Actions",
-        "Terraform",
-        "OCI",
-        "AWS",
-        "GCP",
-        
     ];
     return (
         <section 
@@ -49,18 +24,17 @@ export const About = () => {
             <div className="max-w-3xl mx-auto">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
                 <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                    <p className="text-l text-gray-300 mb-6">
-                        A guy who loves innovations and technology and always eager to learn new things.
-                        <br />  <br /> 
-                        Curently Exploring Blockchain and Web3. 
+                    <p className="text-gray-300 mb-6">
+                        I am a 20-year-old student and self-taught programmer, 
+                        learning JavaScript and Python to build impactful solutions. 
                         
                     </p>
 
                     <div className="grid grid-cols-l md:grid-cols-2 gap-4">
                         <div className="rounded-xl p-3 hover:-translate-y-1 transition-all ">
-                            <h3 className="text-xl font-bold mb-4">Fullstack</h3>
+                            <h3 className="text-xl font-bold mb-4">Frontend</h3>
                             <div className="flex flex-wrap gap-2"> 
-                                {Fullstack.map((tech)=> {
+                                {frontendSkills.map((tech)=> {
                                     return (
                                         <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                         hover:shadow-[0_2px_8px_rgba(59,130,2246,0,2)] transistion ">{tech}</span>
@@ -69,20 +43,9 @@ export const About = () => {
                             </div>
                         </div>
                         <div className="rounded-xl p-3 hover:-translate-y-1 transition-all ">
-                            <h3 className="text-xl font-bold mb-4">Blockchain</h3>
+                            <h3 className="text-xl font-bold mb-4">Backend</h3>
                             <div className="flex flex-wrap gap-2"> 
-                                {Blockchain.map((tech)=> {
-                                    return (
-                                        <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                        hover:shadow-[0_2px_8px_rgba(59,130,2246,0,2)] transistion-all">{tech}</span>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                        <div className="rounded-xl p-3 hover:-translate-y-1 transition-all ">
-                            <h3 className="text-xl font-bold mb-4">Devops</h3>
-                            <div className="flex flex-wrap gap-2"> 
-                                {Devops.map((tech)=> {
+                                {backendSkills.map((tech)=> {
                                     return (
                                         <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                         hover:shadow-[0_2px_8px_rgba(59,130,2246,0,2)] transistion-all">{tech}</span>
@@ -93,20 +56,20 @@ export const About = () => {
                     </div>
                 </div>
                 
-                {/* <div className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-8">*/}
-                <div className="grid grid-cols-1 gap-2 md:grid-cols-1 mt-8"> 
+                
+                <div className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-8">
                     <div className="p-6 rounded-xl border-white/10 border hover:-translate-1 transition-all">
                         <h3 className=" text-xl font-bold mb-4"> Education</h3>
                         <ul className="list-disc list-inside text-gray-300 space-y-2"> 
                             <li>
-                                <strong>B.E in Computer Science and Engineering</strong> - Anand Institute Of Higher Technology (2022-2026)
+                                <strong>B.E in Computer Science</strong> - ANNA University (2022-2026)
                             </li>
-                            {/* <li>
+                            <li>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            </li> */}
+                            </li>
                         </ul>
                     </div>
-                    {/* <div className="p-6 rounded-xl border-white/10 border hover:-translate-1 transition-all">
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-1 transition-all">
                         <h3 className=" text-xl font-bold mb-4"> Work Experiencce</h3>
                         <div className="space-y-4 text-gray-300">
                             <div>
@@ -123,7 +86,7 @@ export const About = () => {
                                 </p>
                             </div>
                         </div>
-                    </div> */}
+                    </div>
                 </div>
             </div>
             </RevealOnScroll>
