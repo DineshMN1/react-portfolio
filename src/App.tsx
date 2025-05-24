c7ec246997d5b9f46b34fc02339a632709f9bfe1
Dinesh M <dineshmuthuswami@gmail.com>
2025-05-24 12:43:04 +0530

    update page

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,28 @@ import { useState } from 'react'
 import { LoadingScreen } from './components/LoadingScreen'
 import './index.css'
 import { Navbar } from './components/Navbar'
-import { Mobileview } from './components/Mobilemenu'
-import Home from './components/sections/Home'
-import { About } from './components/sections/About'
-import { Project } from './components/sections/Projects'
-import { Contact } from './components/sections/Contact'
-import { AuroraBackground } from './components/aurora-background'
+import { Mobileview } from './components/Mobilemenu';
+import Home from './components/sections/Home';
+import { About } from './components/sections/About';
+import { Project } from './components/sections/Projects';
+import { Contact } from './components/sections/Contact';
+
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [menuOpen, setMenuOpen] = useState(false)
-
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <AuroraBackground showRadialGradient>
-        <div
-          className={`min-h-screen transition-opacity duration-700 ${
-            isLoaded ? 'opacity-100' : 'opacity-0'
-          } bg-black text-gray-100 relative z-10`}
-        >
-          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-          <Mobileview menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-          <Home />
-          <About />
-          <Project />
-          <Contact />
-        </div>
-      </AuroraBackground>
+    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+    <div className='min-h-screen transition-opacity duration-700 ${isLoaded ? " opacity-100" : " opacity-0"} bg-black text-gray-100'>
+    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Mobileview menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Home/>
+    <About/>
+    <Project/>
+    <Contact/>
+    </div>
+    
     </>
   )
 }
