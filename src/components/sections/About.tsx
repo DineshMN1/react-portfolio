6c9dd47b576a608a4b0a5d45da750b54c5add740
Dinesh M <dineshmuthuswami@gmail.com>
2025-05-31 03:08:11 +0530

    update portfolio

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,19 +1,44 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 export const About = () => {
-    const frontendSkills = [
+    const Fullstack = [
         "React",
         "Next.js",
         "TypeScript",
         "Tailwind CSS",
-    ];
-
-    const backendSkills = [
-        "Node.js",
-        "python",
-        "Express.js",
-        "AWS",
+        "Express",
         "MongoDB",
         "PostgreSQL",
+        "Git",
+        "Firebase",
+    ];
+
+    const Blockchain = [
+        "Solidity",
+        "Ethereum",
+        "Smart Contracts",
+        "Web3.js",
+        "Ethers.js",
+        "Hardhat",
+        "Truffle",
+        "Ganache",
+        "IPFS",
+        "Solana",
+        "Rust",
+        "Anchor",
+        
+    ];
+
+    const Devops = [
+        "Docker",
+        "Kubernetes",
+        "CI/CD",
+        "Git",
+        "GitHub Actions",
+        "Terraform",
+        "OCI",
+        "AWS",
+        "GCP",
+        
     ];
     return (
         <section 
@@ -24,17 +49,18 @@ export const About = () => {
             <div className="max-w-3xl mx-auto">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
                 <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                    <p className="text-gray-300 mb-6">
-                        I am a 20-year-old student and self-taught programmer, 
-                        learning JavaScript and Python to build impactful solutions. 
+                    <p className="text-l text-gray-300 mb-6">
+                        A guy who loves innovations and technology and always eager to learn new things.
+                        <br />  <br /> 
+                        Curently Exploring Blockchain and Web3. 
                         
                     </p>
 
                     <div className="grid grid-cols-l md:grid-cols-2 gap-4">
                         <div className="rounded-xl p-3 hover:-translate-y-1 transition-all ">
-                            <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                            <h3 className="text-xl font-bold mb-4">Fullstack</h3>
                             <div className="flex flex-wrap gap-2"> 
-                                {frontendSkills.map((tech)=> {
+                                {Fullstack.map((tech)=> {
                                     return (
                                         <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                         hover:shadow-[0_2px_8px_rgba(59,130,2246,0,2)] transistion ">{tech}</span>
@@ -43,9 +69,20 @@ export const About = () => {
                             </div>
                         </div>
                         <div className="rounded-xl p-3 hover:-translate-y-1 transition-all ">
-                            <h3 className="text-xl font-bold mb-4">Backend</h3>
+                            <h3 className="text-xl font-bold mb-4">Blockchain</h3>
                             <div className="flex flex-wrap gap-2"> 
-                                {backendSkills.map((tech)=> {
+                                {Blockchain.map((tech)=> {
+                                    return (
+                                        <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                        hover:shadow-[0_2px_8px_rgba(59,130,2246,0,2)] transistion-all">{tech}</span>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        <div className="rounded-xl p-3 hover:-translate-y-1 transition-all ">
+                            <h3 className="text-xl font-bold mb-4">Devops</h3>
+                            <div className="flex flex-wrap gap-2"> 
+                                {Devops.map((tech)=> {
                                     return (
                                         <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                         hover:shadow-[0_2px_8px_rgba(59,130,2246,0,2)] transistion-all">{tech}</span>
@@ -56,20 +93,20 @@ export const About = () => {
                     </div>
                 </div>
                 
-                
-                <div className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-8">
+                {/* <div className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-8">*/}
+                <div className="grid grid-cols-1 gap-2 md:grid-cols-1 mt-8"> 
                     <div className="p-6 rounded-xl border-white/10 border hover:-translate-1 transition-all">
                         <h3 className=" text-xl font-bold mb-4"> Education</h3>
                         <ul className="list-disc list-inside text-gray-300 space-y-2"> 
                             <li>
-                                <strong>B.E in Computer Science</strong> - ANNA University (2022-2026)
+                                <strong>B.E in Computer Science and Engineering</strong> - Anand Institute Of Higher Technology (2022-2026)
                             </li>
-                            <li>
+                            {/* <li>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-1 transition-all">
+                    {/* <div className="p-6 rounded-xl border-white/10 border hover:-translate-1 transition-all">
                         <h3 className=" text-xl font-bold mb-4"> Work Experiencce</h3>
                         <div className="space-y-4 text-gray-300">
                             <div>
@@ -86,7 +123,7 @@ export const About = () => {
                                 </p>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             </RevealOnScroll>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -13,12 +13,12 @@ export const Project = () =>{
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> Project-1 </h3>
+                    <h3 className="text-xl font-bold mb-2"> Blog site </h3>
                         <p className="text-gray-400 mb-4"> 
-                            Lorem ipsum dolor sit amet,ipsum dolor sit amet
+                            Blog site build with next js and tailwind css for mdx components.
                         </p>
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React","Node","Express","tailwind"].map((tech, key) => (
+                            {["Next js","Typescript","tailwind css","pnpm", "Postgres"].map((tech, key) => (
                                 <span 
                                     key={key} 
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
@@ -30,8 +30,8 @@ export const Project = () =>{
                         </div>
 
                         <div className="flex items-center gap-6 ">
-                            <a href="#proj1" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a>
-                            <a href="#github/proj1"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
+                            <a href="https://dineshmn.fyi" target="_blank" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a>
+                            <a href="https://github.com/DineshMN1/portfolio"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
                         </div>
                         
                     </div>
@@ -43,10 +43,10 @@ export const Project = () =>{
                             transition-all
                             "
                         >
-                        <h3 className="text-xl font-bold mb-4"> Project-2 </h3>
-                            <p className="text-gray-400 mb-4"> Lorem ipsum dolor sit amet,ipsum dolor sit amet </p>
+                        <h3 className="text-xl font-bold mb-4"> Solana Faucet</h3>
+                            <p className="text-gray-400 mb-4"> Sonala Faucet is build to get devnet SOL for testing</p>
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["Next","Express", "Docker", "Nginx"].map((tech, key) => (
+                                {["Next js","Tailwind css", "TypeScript", "Solana", "Solana Wallet"].map((tech, key) => (
                                     <span 
                                         key={key} 
                                         className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
@@ -58,8 +58,8 @@ export const Project = () =>{
                             </div>
 
                         <div className="flex items-center gap-6 ">
-                            <a href="#proj1" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a>
-                            <a href="#github/proj1"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
+                            <a href="https://sol.dineshmn.fyi" target="_blank" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a>
+                            <a href="https://github.com/DineshMN1/Solana-Faucet"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
                         </div>
                         
                     </div>
@@ -68,10 +68,10 @@ export const Project = () =>{
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-8">
 
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition-all">
-                    <h3 className="text-xl font-bold mb-4"> Project-3 </h3>
-                        <p className="text-gray-400 mb-4"> Lorem ipsum dolor sit amet,ipsum dolor sit amet </p>
+                    <h3 className="text-xl font-bold mb-4"> Wall-E</h3>
+                        <p className="text-gray-400 mb-4"> Autonomous Fleet based robot for Agriculture </p>
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["OCI", "Docker",].map((tech, key) => (
+                            {["Next js", "Typescript","Tailwind css", "We bots", "Flask", ].map((tech, key) => (
                                 <span 
                                     key={key} 
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
@@ -83,14 +83,14 @@ export const Project = () =>{
                         </div>
 
                         <div className="flex items-center gap-6 ">
-                            <a href="#proj1" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a>
-                            <a href="#github/proj1"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
+                            {/* <a href="#Wall-E" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a> */}
+                            <a href="https://github.com/DineshMN1/Wall-E"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
                         </div>
                         
                     </div>
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition-all">
-                    <h3 className="text-xl font-bold mb-4"> Project-4 </h3>
-                        <p className="text-gray-400 mb-4"> Lorem ipsum dolor sit amet,ipsum dolor sit amet </p>
+                    <h3 className="text-xl font-bold mb-4"> OLLAMA Custom model </h3>
+                        <p className="text-gray-400 mb-4"> Create custom AI models with LLaMA and Ollama. Easily define unique AI personas and behaviors using a model system file. </p>
                         <div className="flex flex-wrap gap-2 mb-4">
                             {["Python","Assembly"].map((tech, key) => (
                                 <span 
@@ -104,8 +104,8 @@ export const Project = () =>{
                         </div>
 
                         <div className="flex items-center gap-6 ">
-                            <a href="#proj1" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a>
-                            <a href="#github/proj1"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
+                            {/* <a href="#proj1" className=" text-blue-400 hover:text-blue-300 transition-colors "> View Project</a> */}
+                            <a href="https://github.com/DineshMN1/ollama-model-create"className="text-blue-400 hover:text-blue-300 transition-colors">github</a>
                         </div>
                         
                     </div>
